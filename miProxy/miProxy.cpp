e1a306b8934b0bdfe0083e705b8eb0b0ad74556e
byydzh <89905429+byydzh@users.noreply.github.com>
2023-03-17 15:11:41 +0800

    Add files via upload

--- miProxy/miProxy.cpp
+++ miProxy/miProxy.cpp
@@ -65,11 +65,23 @@ int get_server_socket(struct sockaddr_in *address, int listen_port) {
     return server_socket;
 }
 
+// clear is used when you want clear exsit context
+void out2log(string log_name, string browser_ip, string chunkname, string server_ip, 
+            double duration, double tput, double avg_tput, int bitrate, int clear=0){
+    FILE *fp;
+    if(clear)
+        fp = fopen(log_name.c_str(), "w+");
+    else
+        fp = fopen(log_name.c_str(), a);
+    fprintf(fp, "%s %s %s %f %f %f %d\n",
+            browser_ip.c_str(), chunkname.c_str(), server_ip.c_str(), 
+            duration, tput, avg_tput, bitrate);
+
+    fclose(fp);
+}
+
 int main(int argc, const char** argv)
 {
-    //Step0: global variables
-    string message;
-    
     //Step1: check argvs
     if(argc != 6){
         printf("Usage: %s --nodns <listen-port> <www-ip> <alpha> <log>\n",argv[0]);
@@ -92,12 +104,11 @@ int main(int argc, const char** argv)
     int server_socket;
     struct sockaddr_in server_address;
     server_socket = get_server_socket(&server_address, listen_port);
-    
+
     //step3: get client_sockets
     int client_sock;
     int client_sockets[MAX_CLIENTS] = {0};
-    string client_ips[MAX_CLIENTS];
-    
+
     //step4: deal with connections
     fd_set readfds;
     while (1)
@@ -123,11 +134,12 @@ int main(int argc, const char** argv)
             perror("select error");
         }
 
-        // If something happened on the server socket, then its an incoming connection, call accept()
+        // If something happened on the master socket ,
+        // then its an incoming connection, call accept()
         if (FD_ISSET(server_socket, &readfds))
         {
-            int new_socket = accept(server_socket, (struct sockaddr *)&server_address,
-                                    (socklen_t *)&(sizeof(server_address)));
+            int new_socket = accept(server_socket, (struct sockaddr *)&address,
+                                    (socklen_t *)&addrlen);
             if (new_socket < 0)
             {
                 perror("accept");
@@ -135,15 +147,18 @@ int main(int argc, const char** argv)
             }
 
             // inform user of socket number - used in send and receive commands
-            printf("\n---New connection---\n");
+            printf("\n---New host connection---\n");
             printf("socket fd is %d , ip is : %s , port : %d \n", new_socket,
-                   inet_ntoa(server_address.sin_addr), ntohs(server_address.sin_port));
+                   inet_ntoa(address.sin_addr), ntohs(address.sin_port));
 
             // send new connection greeting message
             // TODO: REMOVE THIS CALL TO SEND WHEN DOING THE ASSIGNMENT.
-            //ssize_t send_ret = send(new_socket, message, strlen(message), 0);
-            //printf("Welcome message sent successfully\n");
-            
+            ssize_t send_ret = send(new_socket, message, strlen(message), 0);
+            if (send_ret != strlen(message))
+            {
+                perror("send");
+            }
+            printf("Welcome message sent successfully\n");
             // add new socket to the array of sockets
             for (int i = 0; i < MAXCLIENTS; i++)
             {
@@ -151,12 +166,10 @@ int main(int argc, const char** argv)
                 if (client_sockets[i] == 0)
                 {
                     client_sockets[i] = new_socket;
-                    strcpy(client_ips[i], inet_ntoa(server_address.sin_addr));
                     break;
                 }
             }
         }
-        
         // else it's some IO operation on a client socket
         for (int i = 0; i < MAXCLIENTS; i++)
         {
